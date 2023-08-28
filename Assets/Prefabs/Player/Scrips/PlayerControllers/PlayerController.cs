889dc6b89bc124d6c7c2ada65275682b2135ae4e
juliengrima <gogrinformatique@gmail.com>
2023-08-28 11:22:10 +0200

    Playercamera ok

--- Assets/Prefabs/Player/Scrips/PlayerControllers/PlayerController.cs
+++ Assets/Prefabs/Player/Scrips/PlayerControllers/PlayerController.cs
@@ -52,9 +52,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xAxis = _move.action.ReadValue<Vector2>().x * _speed;     //action move dans axe X uniquement 
+        float xAxis = _move.action.ReadValue<Vector2>().x * _speed;     //action move dans axe X uniquement
         float yAxis = _move.action.ReadValue<Vector2>().y * _speed;     //action move dans axe y uniquement
-        float XYaxis = xAxis + yAxis;                                   //addition des axes de move
+        float XYaxis = xAxis + yAxis;                                   //addition des axes de move pour l'animation
         //Mouvements(XYaxis, xAxis, runXaxis);
         Mouvements(XYaxis);
         //Run();
@@ -81,10 +81,6 @@ public class PlayerController : MonoBehaviour
             _animator.SetBool("IsRunningBool", false);
         }
     }
-    //void Run()
-    //{
-
-    //}
     void Jump()
     {
         _isButtonPressed = _jump.action.WasPressedThisFrame();
@@ -93,7 +89,11 @@ public class PlayerController : MonoBehaviour
             //_rb.AddForce(Vector2.up * _jumpForce);
             _source.PlayOneShot(_AudioJump);
             _animator.SetBool("IsJumping", true);
-        }  
+        }
+        else
+        {
+            _animator.SetBool("IsJumping", false);
+        }
     }
     void Fight()
     {
@@ -102,6 +102,10 @@ public class PlayerController : MonoBehaviour
         {
             _source.PlayOneShot(_AudioFight);
             _animator.SetBool("IsFighting", true);
+        }
+        else
+        {
+            _animator.SetBool("IsFighting", false);
         }
     }
     void UpdateRotation(float xAxis)
