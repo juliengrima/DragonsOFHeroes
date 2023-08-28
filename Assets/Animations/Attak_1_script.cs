2ea5058a841753d5643b52a8615a350c310fc24b
juliengrima <gogrinformatique@gmail.com>
2023-08-28 14:42:12 +0200

    DDD

---
+++ Assets/Animations/Attak_1_script.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attak_1_script : MonoBehaviour
+{	
+    #region Champs
+    #endregion
+    #region Unity LifeCycle
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+    #region Methods
+    void FixedUpdate ()
+    {
+        
+    }
+    void LateUpdate ()
+    {
+        
+    }
+    #endregion
+    #region Coroutines
+    #endregion
+}
