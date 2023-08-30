a0169ee67401b257a6cc7d9efc7d1323f4364cbc
Zpektrum <118989390+ZPEKTRUM@users.noreply.github.com>
2023-08-30 11:12:37 +0200

    Unity Process

---
+++ Assets/CharacterAttack.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterAttack : MonoBehaviour
+{
+
+    [SerializeField] InputActionReference _attackInput;
+    [SerializeField] HitZone _hitZone;
+
+    [SerializeField] Animator _animator;
+    [SerializeField] AudioSource _punchSound;
+
+    private void Update()
+    {
+
+
+        //On check le bouton d' atttack quivient d'être enfoncé
+
+        if (_attackInput.action.WasPressedThisFrame())
+        {
+
+            // C'est le cas on parcours la liste des colliders détectés
+            //Et sur chaque collider on choppe le copsant Health et on appelle
+            //takeDamage dessus.
+
+
+            foreach (Collider2D col in _hitZone.Colliders)
+            {
+
+                Debug.Log($"attack {col.attachedRigidbody.name}");
+
+                //Le composant Health est au même endroit que le RB de notre enemi
+
+                Health h = col.attachedRigidbody.GetComponent<Health>();
+                h.TakeDamage();
+
+                {
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Assets/HitZone.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitZone : MonoBehaviour
+{
+    public IEnumerable<Collider2D> Colliders { get; internal set; }
+
+}
